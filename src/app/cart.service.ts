13e638df3e5458f9a579fe78711e91cf71dfa399
phiwesriva <phiwes@icloud.com>
2020-01-04 15:28:13 +0200

    6 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
+  items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 
   constructor() { }
 
