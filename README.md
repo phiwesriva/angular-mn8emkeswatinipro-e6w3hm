12385df4c493f6976a4439ae2b976309222e4204
phiwesriva <phiwes@icloud.com>
2020-01-04 13:05:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mn8emkeswatinipro-e6w3hm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mn8emkeswatinipro-e6w3hm)
---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    name: 'Vol 1| Issue 1 | Apr-Jun 2018',
+    price: 799,
+    description: 'Interview with SNHB CEO Mduduzi Dlamini'
+  },
+  {
+    name: 'Vol 1| Issue 2 | Jul-Sep 2018',
+    price: 699,
+    description: 'SNPF GM Finance & Business Development Langalakhe Dlamini'
+  },
+  {
+    name: 'Vol 1| Issue 3 | Oct-Dec 2018',
+    price: 299,
+    description: 'One on One Moses Motsa'
+  },
+   {
+    name: 'Vol 1| Issue 4 | Oct-Dec 2018',
+    price: 299,
+    description: 'Busi Mayisela talks Property & Business'
+  },
+   {
+    name: 'Vol 1| Issue 5 | Oct-Dec 2018',
+    price: 299,
+    description: 'Matata Group CEO unpacka opportunities in Eswatini'
+  }
+ ];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
