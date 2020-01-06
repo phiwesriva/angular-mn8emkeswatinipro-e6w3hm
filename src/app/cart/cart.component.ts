b04bac8bea58345c3ffcac5843c88c8f75420047
phiwesriva <phiwes@icloud.com>
2020-01-06 11:41:47 +0200

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,7 +8,7 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-iterms;
+items;
   constructor(private cartService: CartService) { }
 
   ngOnInit() {this.items = this.cartService.getItems();
